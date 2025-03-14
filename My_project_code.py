5133bf7bf3011077c51f27385fb0b132bdcc316e
Richard Reynolds <RReynolds1@sheffield.ac.uk>
2025-03-14 16:50:50 +0000

    Add files via upload

--- My_project_code.py
+++ My_project_code.py
@@ -118,63 +118,6 @@ class RunBuilder():
 		return runs
 
 class RunManager():
-	def __init__(self):
-
-		self.epoch_count = 0
-		self.epoch_loss = 0
-		self.epoch_num_correct = 0
-		self.epoch_start_time = None
-
-		self.run_params = None
-		self.run_count = 0
-		self.run_data = []
-		self.run_start_time = None
-
-		self.network = None
-		self.loader = None
-		self.tb = None
-
-	def begin_run(self, run, network, loader):
-		self.run_start_time = time.time()
-		self.run_params = run
-		self.run_count += 1
-		self.network = network
-		self.loader = loader
-		self.tb = SummaryWriter(comment=f'-{run}')
-		characteristics, labels = next(iter(self.loader))
-
-	def end_run(self):
-		self.tb.close()
-		self.epoch_count = 0
-
-	def begin_epoch(self):
-		self.epoch_start_time = time.time()
-		self.epoch_count += 1
-		self.epoch_loss = 0
-		self.epoch_num_correct = 0
-
-	def end_epoch(self):
-		epoch_duration = time.time() - self.epoch_start_time
-		run_duration = time.time() - self.run_start_time
-
-		loss = self.epoch_loss / len(self.loader.dataset)
-		accuracy = self.epoch_num_correct / len(self.loader.dataset)
-
-		self.tb.add_scalar('Loss', loss, self.epoch_count)
-		self.tb.add_scalar('Accuracy', accuracy, self.epoch_count)
-
-	def _get_num_correct(self, preds, labels):
-		return preds.argmax(dim=1).eq(labels).sum().item()
-
-	def track_loss(self, loss, batch):
-		self.epoch_loss += loss.item() * batch[0].shape[0]
-
-	def track_num_correct(self, preds, labels):
-		self.epoch_num_correct += self._get_num_correct(preds, labels)
-
-	def inform(self, discrete_n):
-		if self.epoch_count % discrete_n == 0:
-			print(self.epoch_count, ' ', self.run_count)
 	def save_checkpoint(self, model, optimizer, epoch, filename="checkpoint.pth"):
 		print(f"Saving checkpoint at epoch {epoch}")
 		torch.save({'epoch': epoch, 'model_state_dict':model.state_dict(),'optimizer_state_dict': optimizer.state_dict(),}, filename)
